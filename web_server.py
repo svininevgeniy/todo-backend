318facc452ae8ed5c3ede8596038000a3794b578
Карина <karina@MacBook-Air-Karina.local>
2024-02-13 20:39:24 +0300

    fix incorrect logic

--- web_server.py
+++ web_server.py
@@ -37,6 +37,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 
 if __name__ == '__main__':
